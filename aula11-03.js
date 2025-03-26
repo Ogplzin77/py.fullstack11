f2d89e45c50082f89136512bf5bc90a0d437b149
Og77pl <pabllonunes010@gmail.com>
2025-03-26 15:38:28 -0300

    Update aula11-03.js

--- aula11-03.js
+++ aula11-03.js
@@ -1,10 +1,1 @@
- ////////////////////  Sistema de Reservas de Hotel////////////////////////
- 
- class Hotel {
-    constructor (CvHotel, Nova Iguacu, quartosLivres) {
-        this CvHotel =  CvHotel;
-        this Nova Iguacu = Nova Iguacu;
-        this quartosLivres = quartosLivres;
-    
-    }
- 
+

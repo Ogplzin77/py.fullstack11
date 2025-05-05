29b22cbb5010f7c4544876e68de1d8c0308714f5
Og77pl <pabllonunes010@gmail.com>
2025-05-05 13:50:19 -0300

    Delete UC3 Back-And/sintaxe.py

--- UC3 Back-And/sintaxe.py
+++
@@ -1,9 +0,0 @@
-#comentarios de uma linda 
-""""
-comentarios de multiplas linhas (na verdade, são strings não atribuidas)
-"""
-#exemplos de função básica
-def saudacao(nome): #função de saudacao
-    return f"Ola, {nome}!" #retorna uma string
-
-print(saudacao("Pabllo")) # são saida: olá, Pabllo!
